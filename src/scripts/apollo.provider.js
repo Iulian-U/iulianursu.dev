4913875e71b6ae0a4d8132f989adc73dab08e396
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-08 17:10:32 +0200

    Update apollo.provider.js

--- src/scripts/apollo.provider.js
+++ src/scripts/apollo.provider.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 import { createApolloProvider } from "@vue/apollo-option";
 
 const cache = new InMemoryCache();
-const api = process.env.VITE_GRAPHCMS_API;
+const api = process.env.GRAPHCMS_API;
 
 const client = new ApolloClient({
   cache,
