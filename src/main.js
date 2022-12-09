1d57b1cd3a1872d69d4db2282c0f3156e3535703
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-09 17:57:20 +0200

    fixed analytics

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import Particles from "vue3-particles";
+import { inject } from "@vercel/analytics";
 
 import "virtual:windi.css";
 import "./assets/css/global.css";
@@ -16,5 +17,6 @@ app.use(Particles);
 app.use(createPinia());
 app.use(router);
 app.use(apolloProvider.provider);
+app.use(inject);
 
 app.mount("#app");
