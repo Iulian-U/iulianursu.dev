821702c3e971c1178ce3f280d74c67bb220bd43b
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-09 18:09:25 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import App from "./App.vue";
 import router from "./router";
 
 import Particles from "vue3-particles";
-import { inject } from "@vercel/analytics";
 
 import "virtual:windi.css";
 import "./assets/css/global.css";
@@ -17,6 +16,5 @@ app.use(Particles);
 app.use(createPinia());
 app.use(router);
 app.use(apolloProvider.provider);
-app.use(inject);
 
 app.mount("#app");
