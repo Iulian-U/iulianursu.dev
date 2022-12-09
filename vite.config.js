f3741bc413c41ee13a43ad1e47339a07d924494e
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-09 17:49:30 +0200

    added Vercel analytics

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
+import inject from "./src/plugins/vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [vue(), WindiCSS(), inject()],
   base: "./",
   resolve: {
     alias: {
